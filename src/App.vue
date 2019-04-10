bd06495da5bc52191f4bf0771ba0a9e6bc9622f8
Douglas Hoang <douglashoang@Douglass-MacBook-Pro.local>
2019-04-09 19:48:22 -0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-tabs v-model="active" color="cyan" dark slider-color="yellow">
+      <v-tab v-for="(tab, index) in tabs" :key="index" ripple>{{ tab }} </v-tab>
+      <v-tab-item :key="0">
+        <div>
+          <spice></spice>
+        </div>
+      </v-tab-item>
+
+      <v-tab-item :key="1">
+        <v-card flat>
+          <v-card-text>
+            hello
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+
+      <v-tab-item :key="2">
+        <v-card flat>
+          <v-card-text>
+            dude
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Spice from './components/Spice'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Spice,
+  },
+  data() {
+    return {
+      active: null,
+      tabs: ['Grind', 'History', 'Configuration'],
+      text:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Spice.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-container grid-list-xl text-xs-center>
+    <v-stepper v-model="e1">
+      <v-stepper-header>
+        <v-stepper-step :complete="e1 > 1" step="1"
+          >Select a Spice</v-stepper-step
+        >
+
+        <v-divider></v-divider>
+
+        <v-stepper-step :complete="e1 > 2" step="2"
+          >Select Amount</v-stepper-step
+        >
+      </v-stepper-header>
+
+      <v-stepper-items flat>
+        <v-stepper-content step="1">
+          <v-layout row wrap>
+            <v-flex v-for="(spice, index) in spices" :key="index" xs4>
+              <v-card
+                class="card-style"
+                height="100px"
+                @click="toggleSpice(spice.id)"
+                :color="spice.selected ? '#42A5F5' : undefined"
+                ripple
+              >
+                <div class="spice-name">
+                  {{ spice.name }}
+                </div>
+              </v-card>
+            </v-flex>
+          </v-layout>
+          <v-btn color="primary" @click="e1 = 2">
+            Next
+          </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <v-card class="mb-5" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 1">
+            Grind
+          </v-btn>
+
+          <v-btn flat @click="e1 = 1">Cancel</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'spice',
+  data() {
+    return {
+      e1: 0,
+      spices: [
+        {
+          id: 0,
+          name: 'Salt',
+          value: 0,
+          selected: false,
+        },
+        {
+          id: 1,
+          name: 'Pepper',
+          value: 0,
+          selected: false,
+        },
+        {
+          id: 2,
+          name: 'Paprika',
+          value: 0,
+          selected: false,
+        },
+        {
+          id: 3,
+          name: 'Cinnamon',
+          value: 0,
+          selected: false,
+        },
+        {
+          id: 4,
+          name: 'Peppers',
+          value: 0,
+          selected: false,
+        },
+        {
+          id: 5,
+          name: 'Salt',
+          value: 0,
+          selected: false,
+        },
+      ],
+    }
+  },
+  methods: {
+    toggleSpice(id) {
+      this.spices = this.spices.map(spice => {
+        if (spice.id === id) {
+          return {
+            ...spice,
+            selected: !spice.selected,
+          }
+        }
+
+        return spice
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.card-style {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.spice-title {
+  font-size: 16px;
+}
+.spice-name {
+  font-size: 14px;
+}
+</style>
